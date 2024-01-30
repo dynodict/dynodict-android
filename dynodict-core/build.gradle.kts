214f4bd46c1d9093ee93420b11d3738f1e24d2c8
mkovalyk <mishakovalyk@gmail.com>
2024-01-30 10:01:50 +0200

    Initial version of github Packages version

--- dynodict-core/build.gradle.kts
+++ dynodict-core/build.gradle.kts
@@ -2,15 +2,37 @@ plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("maven-publish")
+    `maven-publish`
 }
 
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            group = "org.dynodict"
+//            artifactId = "library-core"
+//            version = Versions.dynodictLibrary
+//            from(components["java"])
+//        }
+//    }
+//}
+
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
+    repositories {
+        maven {
+            name = "dynodict-android"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = "mkovalyk"
+                password = "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"
+//                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+//                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
             group = "org.dynodict"
-            artifactId = "library-core"
             version = Versions.dynodictLibrary
+        }
+    }
+    publications {
+        register<MavenPublication>("org.dynodict") {
             from(components["java"])
         }
     }
